ff84ec84c332c2b2d0c8500667fed49667789a53
BLETOUS <bletous@gmail.com>
2022-05-19 09:33:02 +0200

    Update ip_Ref and add MX

--- Mx/object.go
+++ Mx/object.go
@@ -7,6 +7,8 @@ import (
 
 const apiName = "mx"
 
+var isUsable = false
+
 type Mx struct {
 	Id        int    `json:"mx_id,omitempty"`
 	Name      string `json:"mx_name"`
@@ -24,14 +26,16 @@ func init() {
 	mapJson["mx_name"] = types.String
 	mapJson["is_updatable"] = types.Bool
 	mapJson["is_removable"] = types.Bool
-	mapJson["last_update"] = types.Bool
 	// Name Struct
 	mapName = make(map[string]types.BasicKind)
 	mapName["Id"] = types.Int
 	mapName["Name"] = types.String
 	mapName["Updatable"] = types.Bool
 	mapName["Removable"] = types.Bool
-	// Check on first load if current object is the same that the production
+	// Check on first load if current object is the same that the production and set to usable = true
 	err := checkStruct()
-	fmt.Println(err)
+	if err == nil {
+		isUsable = true
+	}
+	fmt.Println("Usable", isUsable)
 }
